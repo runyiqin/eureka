eb79011959e1dddff6ff6640d6803c4f4ed50a4a
179835947@qq.com <179835947@qq.com>
2019-06-13 11:31:17 +0800

    分支测试，提交feign的hystrix仪表盘功能

--- service-feign/src/main/java/com/example/demo/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/example/demo/ServiceFeignApplication.java
@@ -1,15 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.service.HelloService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class ServiceFeignApplication {
 
 	public static void main(String[] args) {
