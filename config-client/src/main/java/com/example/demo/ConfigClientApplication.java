b3884c9f18b19bc48d15ac70b85b8a8ad275e92b
179835947@qq.com <179835947@qq.com>
2019-06-14 15:20:14 +0800

    添加config-client的配置信息

--- config-client/src/main/java/com/example/demo/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -1,15 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class ConfigClientApplication {
 
+	@Value("${bar}")
+	String bar;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
+	@RequestMapping(value="/hello",method = RequestMethod.GET)
+	public String hello(){
+		return "git中返回的配置信息"+bar;
+	}
 }
