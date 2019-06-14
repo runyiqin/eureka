0ed05aca94e91149195488920a3824118d1fe2d6
179835947@qq.com <179835947@qq.com>
2019-06-14 16:38:14 +0800

    配置高可用的分布式配置中心

--- config-client/src/main/java/com/example/demo/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -3,12 +3,14 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	@Value("${bar}")
